88490a3adf0b452b725bc0ec3ea36596b1dbcd5a
Adrian <asoliz241@west-mec.org>
2021-02-26 16:20:26 -0700

    Added Pug Files and edited some
    Change the route for the index create form to go to the form

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog')
 
 var app = express();
+
+
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = "mongodb+srv://AdrianMarcus2021:MarcusAdrian2021@rms.mhtob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var mongoDB = process.env.URI || dev_db_url;
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
